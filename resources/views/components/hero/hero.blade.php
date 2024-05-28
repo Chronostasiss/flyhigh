049caef0ac33b2b53e8b3eba8e95facefd0cfbce
Chronostasiss <ahmad.nizham416@gmail.com>
2024-05-28 23:41:38 +0800

    first commit

---
+++ resources/views/components/hero/hero.blade.php
@@ -0,0 +1,24 @@
+<div class="hero z-[1] w-full h-[100vh] grid place-items-center bg-[#141b2b] relative">
+    <div class="flex md:flex-row flex-col items-center w-full md:px-[200px] px-8 justify-between md:gap-0 gap-28">
+        <div class="flex flex-col gap-3 left-animation w-full">
+            <span class="text-blue-400 text-[25px] font-medium">
+                Welcome To fly high
+            </span>
+            <span class="text-white font-medium md:text-[30px] text-[35px]">
+                Luxury Experience <br /> With Our Services.
+            </span>
+            <span class="text-white leading-7 max-w-[300px] text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nulla ipsa unde inventore minus commodi saepe? Eos cumque aliquam consequatur id optio dolorum modi quod?
+            </span>
+            <div class="flex items-center gap-7 mt-5">
+                <button class="bg-blue-400 px-6 py-3 text-gray-900 font-semibold rounded-full">
+                    Book Flight
+                </button>
+                <button class="border-[2px] border-white-100 px-6 py-3 text-white font-semibold rounded-full">
+                    Contact Us
+                </button>
+            </div>
+        </div>
+        <img src="images/plane_hero.png" class="md:w-[100%] w-full right-animation" alt="">
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Airline Booking System</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    @include('components.navbar.navbar')
+    <div>
+        @include('components.hero.hero')
+        @include('components.about.about')
+        @include('components.features.features')
+    </div>
+</body>
+</html>
+
+<!-- http://127.0.0.1:8000/ -->
+<!-- http://127.0.0.1:5500/ -->
